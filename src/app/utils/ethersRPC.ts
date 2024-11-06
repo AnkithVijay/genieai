43ae1f0230b6712ce38106c3d9fec16ebc108918
AnkithVijay <vijay2ankith@gmail.com>
2024-11-06 17:23:11 +0530

    fixed ethers issue

--- src/app/utils/ethersRPC.ts
+++ src/app/utils/ethersRPC.ts
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 
 const getChainId = async (provider: IProvider): Promise<any> => {
     try {
-        const ethersProvider = new ethers.BrowserProvider(provider);
+        const ethersProvider = new ethers.providers.Web3Provider(provider);
         // Get the connected Chain's ID
         const networkDetails = await ethersProvider.getNetwork();
         return networkDetails.chainId.toString();
@@ -15,8 +15,8 @@ const getChainId = async (provider: IProvider): Promise<any> => {
 
 const getAccounts = async (provider: IProvider): Promise<any> => {
     try {
-        const ethersProvider = new ethers.BrowserProvider(provider);
-        const signer = await ethersProvider.getSigner();
+        const ethersProvider = new ethers.providers.Web3Provider(provider);
+        const signer = ethersProvider.getSigner();
 
         // Get user's Ethereum public address
         const address = signer.getAddress();
@@ -29,14 +29,14 @@ const getAccounts = async (provider: IProvider): Promise<any> => {
 
 const getBalance = async (provider: IProvider): Promise<string> => {
     try {
-        const ethersProvider = new ethers.BrowserProvider(provider);
-        const signer = await ethersProvider.getSigner();
+        const ethersProvider = new ethers.providers.Web3Provider(provider);
+        const signer = ethersProvider.getSigner();
 
         // Get user's Ethereum public address
         const address = signer.getAddress();
 
         // Get user's balance in ether
-        const balance = ethers.formatEther(
+        const balance = ethers.utils.formatEther(
             await ethersProvider.getBalance(address) // Balance is in wei
         );
 
@@ -48,20 +48,24 @@ const getBalance = async (provider: IProvider): Promise<string> => {
 
 const sendTransaction = async (provider: IProvider): Promise<any> => {
     try {
-        const ethersProvider = new ethers.BrowserProvider(provider);
-        const signer = await ethersProvider.getSigner();
+        const ethersProvider = new ethers.providers.Web3Provider(provider);
+        const signer = ethersProvider.getSigner();
 
         const destination = "0x40e1c367Eca34250cAF1bc8330E9EddfD403fC56";
 
-        const amount = ethers.parseEther("0.001");
+        const amount = ethers.utils.parseEther("0.001");
         const fees = await ethersProvider.getFeeData()
+
+        if (!fees.maxFeePerGas || !fees.maxPriorityFeePerGas) {
+            throw new Error("Failed to get fee data");
+        }
 
         // Submit transaction to the blockchain
         const tx = await signer.sendTransaction({
             to: destination,
             value: amount,
-            maxPriorityFeePerGas: fees.maxPriorityFeePerGas, // Max priority fee per gas
-            maxFeePerGas: fees.maxFeePerGas, // Max fee per gas
+            maxPriorityFeePerGas: fees.maxPriorityFeePerGas!, // Max priority fee per gas
+            maxFeePerGas: fees.maxFeePerGas!, // Max fee per gas
         });
 
         // Wait for transaction to be mined
@@ -77,11 +81,11 @@ const signMessage = async (provider: IProvider): Promise<any> => {
     try {
         // For ethers v5
         // const ethersProvider = new ethers.providers.Web3Provider(provider);
-        const ethersProvider = new ethers.BrowserProvider(provider);
+        const ethersProvider = new ethers.providers.Web3Provider(provider);
 
         // For ethers v5
         // const signer = ethersProvider.getSigner();
-        const signer = await ethersProvider.getSigner();
+        const signer = ethersProvider.getSigner();
         const originalMessage = "YOUR_MESSAGE";
 
         // Sign the message
