502c6c3cd270e8702fa5403a0d4b213d376dcb9f
Rohan Reddy <rohanreddy888@gmail.com>
2024-11-16 17:14:47 +0700

    Added Home animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { useWeb3Auth } from "./providers/web3Init";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import BoxReveal from "@/components/ui/box-reveal";
 
 export default function Home() {
   const { login, logout, loggedIn } = useWeb3Auth();
@@ -16,26 +17,38 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <ul className="background">
+        {new Array(8).fill(null).map((_, i) => (
+          <li key={i}></li>
+        ))}
+      </ul>
       <main className="flex flex-col justify-center items-center w-full max-w-3xl gap-12 row-start-2">
         <Image src="/logo/logo1.svg" alt="logo" width={300} height={300} />
         <div className="flex flex-col justify-center items-center gap-4">
-          <h1 className="font-bold text-primary text-4xl">
-            Your AI-Powered Crypto Assistant
-          </h1>
-          <p className="text-lg text-gray-500">
-            Discover the magic of crypto with <i>GenieFi!</i> Swap tokens, check
-            balances, bridge assets, and track transactions seamlessly with our
-            AI-powered assistant. Designed for beginners and pros alike.
-          </p>
+          <BoxReveal boxColor={"#892BE1"} duration={0.1}>
+            <h1 className="font-bold text-primary text-4xl">
+              Your AI-Powered Crypto Assistant
+            </h1>
+          </BoxReveal>
+          <BoxReveal boxColor={"#6b7280"} duration={0.1}>
+            <p className="text-lg text-gray-500">
+              Discover the magic of crypto with <i>GenieFi!</i> Swap tokens,
+              check balances, bridge assets, and track transactions seamlessly
+              with our AI-powered assistant. Designed for beginners and pros
+              alike.
+            </p>
+          </BoxReveal>
         </div>
         <div className="flex justify-end gap-4">
           {!loggedIn ? (
-            <button
-              onClick={login}
-              className="px-6 py-2.5 bg-primary/20 text-primary rounded-full hover:bg-primary/30 font-semibold text-lg border border-primary/50 hover:border-primary"
-            >
-              Login with Web3Auth
-            </button>
+            <BoxReveal boxColor={"#892BE1"} duration={0.1}>
+              <button
+                onClick={login}
+                className="px-6 py-2.5 bg-primary/20 text-primary rounded-full hover:bg-primary/30 font-semibold text-lg border border-primary/50 hover:border-primary"
+              >
+                Login with Web3Auth
+              </button>
+            </BoxReveal>
           ) : (
             <button
               onClick={logout}
